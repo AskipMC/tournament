53877b4a98388dacfe1b812ea19b097fb33d9a86
Ov-qyamm <arya57410@gmail.com>
2023-04-04 11:55:36 +0200

    test fonctionnement git

--- vuejs-frontend/src/views/TournoisView.vue
+++ vuejs-frontend/src/views/TournoisView.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        Tournois
+    <div class="tournament">
+        <div class="tournament_list box">
+            <b-field grouped>
+                <router-link to="tournament_link">Nom Tournoi</router-link>
+            </b-field>
+        </div>
     </div>
+    
 </template>
+
+<style>
+
+.tournament_list{
+    width : 15em;
+    margin: 3em;
+}
+</style>
