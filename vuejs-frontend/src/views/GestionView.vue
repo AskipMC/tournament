fbb270b4129f0b8708773220850866625300a5a8
Ov-qyamm <arya57410@gmail.com>
2023-04-06 08:56:20 +0200

    Style des pages gestion et tournoi

--- vuejs-frontend/src/views/GestionView.vue
+++ vuejs-frontend/src/views/GestionView.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        Gestion
+    <div class="gestion">
+        <p class="gestion_title">Création:</p>
+        <p class="gestion_title">Mes tournois:</p>
+        <div class="gestion_list box">
+            <b-field grouped>
+                <router-link to="gestion_tournament_link">Mon Tournoi</router-link>
+            </b-field>
+        </div>
+        <div class="gestion_list box">
+            <b-field grouped>
+                <router-link to="gestion_tournament_link2">Mon Tournoi 2</router-link>
+            </b-field>
+        </div>
     </div>
 </template>
+
+<style>
+
+.gestion_list{
+    width : 15em;
+    margin: 3em;
+    margin-top: 1.5em;
+}
+.gestion_title{
+    width:15em;
+    font-weight:bold;
+    font-size: 1.5em;
+    margin-left: 2em;
+    margin-top: 2em;
+}
+
+</style>
--- vuejs-frontend/src/views/TournoisView.vue
+++ vuejs-frontend/src/views/TournoisView.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="tournament">
+        <p class="tournament_list_title">Liste des tournois:</p>
         <div class="tournament_list box">
             <b-field grouped>
                 <router-link to="tournament_link">Nom Tournoi</router-link>
+            </b-field>
+        </div>
+        <div class="tournament_list box">
+            <b-field grouped>
+                <router-link to="tournament_link2">Nom Tournoi 2</router-link>
             </b-field>
         </div>
     </div>
@@ -14,5 +20,14 @@
 .tournament_list{
     width : 15em;
     margin: 3em;
+    margin-top: 1.5em;
 }
+.tournament_list_title{
+    width:15em;
+    font-weight:bold;
+    font-size: 1.5em;
+    margin-left: 2em;
+    margin-top: 2em;
+}
+
 </style>
